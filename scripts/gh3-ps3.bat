c154c46eb6d53b5cb6e81e4d878090d6e783ef9d
MetricCepheid <rakleppen@gmail.com>
2026-03-28 20:20:28 -0500

    Changes Galore Readme changes Actions!

---
+++ scripts/gh3-ps3.bat
@@ -0,0 +1,3 @@
+@echo off
+cd ../onyx-command-line-20251011-windows-x64
+onyx.exe pkg UP0010-BLUS30074_00-GUITARHERO3PERM ../GH3/PKG --to ../COMPILED_PS3/GH3PERMA.pkg
---
+++ scripts/gh3-xbox.bat
@@ -0,0 +1,2 @@
+@echo off
+echo Nothing here yet :3
